6cae9687e040fe9f6a947aac4b84d5a77284549f
Kamil Kłys <kamil.klys@gft.com>
2021-02-09 12:29:59 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # spring-rest-examples
+
+# Running application
+Simply run main method in RestApplication.java.
+
+# How to use application
+
+Application operates on some very simple and dummy objects representing `Accounts` entity.
+REST API supports multiple operations.
+
+## Create account resource
+````
+POST /api/accounts
+{
+	"balance":1234.0,
+	"owner":"b4167fba-5ed8-44c0-bc3c-c8dbf94c4092"
+}
+````
+
+## Get account resource
+````
+GET /api/accounts/<id>
+````
+
+## Get all account resources
+````
+GET /api/accounts
+````
+This endpoint supports filtering by Account ID:
+````
+GET /api/accounts?id=<id>
+````
+
+## Replace account resource
+````
+PUT /api/accounts/<id>
+{
+    "id": "152e5972-754f-46b1-9025-d3a2f7fd548d",
+    "balance": 1234,
+    "owner": "b4167fba-5ed8-44c0-bc3c-c8dbf94c4092"
+}
+````
+
+## Update account resource
+````
+PATCH /api/accounts/<id>
+[
+	{
+    	"op": "replace",
+	    "path": "/balance",
+    	"value": 55555
+	}
+]
+````
+``
+Document section to be updated, in order to provide description for other update operations
+``
+
+## Delete account resource
+````
+DELETE /api/accounts/<id>
+````
--- src/main/java/org/example/controller/AccountController.java
+++ src/main/java/org/example/controller/AccountController.java
@@ -7,6 +7,7 @@ import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
 import org.example.model.Account;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,25 +15,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     private List<Account> accounts = new LinkedList<>();
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @GetMapping
-    public List<Account> getAccounts() {
-        return accounts;
-    }
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     @PostMapping
     public Account createAccount(@RequestBody Account accountBody) {
@@ -44,8 +48,7 @@ public class AccountController {
     @PutMapping
     public Account updateAccount(@RequestBody Account accountBody) {
         if (accountBody.getId() != null) {
-            Account account = getAccounts().stream().filter(a -> a.getId().equals(accountBody.getId())).findFirst()
-                    .orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Account with given ID not found"));
+            Account account = getAccountsXml(accountBody.getId()).get(0);
             account.setOwner(accountBody.getOwner());
             account.setBalance(accountBody.getBalance());
             return account;
@@ -58,14 +61,13 @@ public class AccountController {
 
     @PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
     public Account patchAccount(@RequestBody JsonPatch patch, @PathVariable UUID id) throws JsonPatchException, JsonProcessingException {
-        Account account = getAccounts().stream().filter(a -> a.getId().equals(id)).findFirst()
-                .orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Account with given ID not found"));
+        Account account = getAccountsXml(id).get(0);
         return updateAccountCollection(applyPatchToAccount(patch, account));
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable UUID id) {
-        getAccounts().removeIf(a -> a.getId().equals(id));
+        getAccountsXml(null).removeIf(a -> a.getId().equals(id));
     }
 
     private Account applyPatchToAccount(JsonPatch patch, Account account) throws JsonPatchException, JsonProcessingException {
@@ -73,8 +75,31 @@ public class AccountController {
         return objectMapper.treeToValue(patched, Account.class);
     }
 
-    private Account getAccount(UUID id) {
-        return this.accounts.stream().filter(a -> a.getId().equals(id)).findFirst().orElse(null);
+    @PostMapping(value = "/top/up/{id}", consumes = MediaType.TEXT_PLAIN_VALUE)
+    public UUID changeAccountBalanceAsync(@PathVariable UUID id, @RequestBody String amount) {
+        executorService.submit(() -> {
+            try {
+                Thread.sleep(10000);
+                getAccountsXml(id).get(0).setBalance(Integer.parseInt(amount));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        return id;
+    }
+
+    @GetMapping(value = "/{accountId}")
+    public Account getAccount(@PathVariable UUID accountId) throws HttpClientErrorException {
+        return this.accounts.stream().filter(a -> a.getId().equals(accountId)).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Account with given ID not found"));
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public List<Account> getAccountsXml(@RequestParam(required = false) UUID id) {
+        if (id != null) {
+            return this.accounts.stream().filter(a -> a.getId().equals(id)).collect(Collectors.toList());
+        }
+        return accounts;
     }
 
     private Account addAccount(Account account) {
